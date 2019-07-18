ea73a8a21973eef3fd63f5bd7e5db88f8618ba35
VT <vera.thomas@student.uni-tuebingen.de>
2019-07-18 04:40:53 +0200

    test

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -10,14 +10,40 @@ var manualSendResults = true;
 
 // Some default settings of the parameters
 var defaults = [
-    "Message",
-    {
-        transfer: "keypress"
+   "Message", {
+       transfer: "keypress"
+      },
+  "AcceptabilityJudgment", {
+        as: ["1", "2", "3", "4", "5", "6", "7"],
+        presentAsScale: true,
+        instructions: "Zum Antworten Zahl drücken oder anklicken.",
+        leftComment: "(Gar nicht angemessen)", rightComment: "(Angemessen)"
     },
-    "DynamicQuestion", 
-	{
-        clickableAnswers: false    // Prevents participants from choosing an answer by clicking on it
-    },
+
+    //"DynamicQuestion", {
+      //  answers: {
+            // The labels are defined in global_z.css
+        //    CompUnnatural: ["1", "1"],
+          //  Unnatural: ["2", "2"],
+        //    NotNatural: ["3", "3"],
+        //    Average: ["4", "4"],
+        //    Natural: ["5", "5"],
+        //    QuiteNatural: ["6", "6"],
+        //    CompNatural: ["7", "7"]
+        }
+    }
+
+];
+
+//var defaults = [
+//    "Message",
+//    {
+//        transfer: "keypress"
+//    },
+//    "DynamicQuestion",
+//	{
+//        clickableAnswers: false    // Prevents participants from choosing an answer by clicking on it
+//    },
 ];
 
 // Indicate where to find the ZIP files containing the images and the audio samples
@@ -38,29 +64,29 @@ var items = [
 
     // Warn participants audio is requried, give them the opportunity to adjust their volume
     ["audio", "Message", {html: {include: "audio.html"}, transfer: "click"}],
-    
+
     // Increment the counter (set in shuffleSequence when it should happen)
     ["counter", "__SetCounter__", {}],
 
     // Send the results to the server (set in shuffleSequence when it should happen))
     ["results", "__SendResults__", {}],
-	
-	 ["comments", "Form",  {html: {include: "ProlificFeedbackPreConfirmation.html"}}],
-    
-    ["debriefing", "Message", {html: {include: "Debriefing.html"}}],
-    
-    // Two-screen item: task-oriented instructions on the first screen, mouse instructions on the second screen
-    ["instructions", "Message", {html: {include: "instructions.html"}}],  
-	
-    ["consent", "Form", {html: {include: "consentForm.html"}, continueOnReturn:true} ], 
 
-["intro", "Form", {html: {include: "example_intro.html"}}],    
+	 ["comments", "Form",  {html: {include: "ProlificFeedbackPreConfirmation.html"}}],
+
+    ["debriefing", "Message", {html: {include: "Debriefing.html"}}],
+
+    // Two-screen item: task-oriented instructions on the first screen, mouse instructions on the second screen
+    ["instructions", "Message", {html: {include: "instructions.html"}}],
+
+    ["consent", "Form", {html: {include: "consentForm.html"}, continueOnReturn:true} ],
+
+["intro", "Form", {html: {include: "example_intro.html"}}],
 
     // This checks that the resources have been preloaded
     ["preload", "ZipPreloader", {}],
-      
+
     ["sepPractice", "Separator", {normalMessage: "Sehr gut, lassen Sie uns mit einem weiteren Übungsdurchgang fortfahren. Drücken Sie eine beliebige Taste."}],
-      
+
     ["preExp", "Message", {html: "<div style='text-align: center;'><p>Danke! Vergessen Sie im Folgenden nicht, "+
                                  "die Sätze und Situationen ausreichend zu erwägen,<br />"+
                                  "sodass Sie ein informierte Entscheidung treffen können.</p>"+
@@ -95,6 +121,52 @@ var items = [
                     ];
                   }
                 }
-          
+
       ],
+
+      //new
+
+    ].concat(GetItemsFrom(data, null, {
+      ItemGroup: ["item", "group"],
+      Elements: [
+          // Label each item with the value in 'expt'
+          function(x){return x.expt;},
+          // Each item generated from the table is a DynamicQuestion trial
+          "DynamicQuestion",
+          {
+              // We store the values of these cells in the results file
+              legend: function(x){ return [x.item,x.expt,x.condition,x.group,x.sentence].join("+"); },
+              // Generate each picture and return them as answers
+              answers: {"image",  "AcceptabilityJudgment",
+            {s:"From a scale of 1 to 9, how has your morning been?",
+                                               as:["1","2","3","4","5","6","7","8","9"],
+                                                leftComment: "(Bad)",
+                                                rightComment: "(Good)"}],
+             enabled: false,                             // The user won't validate the trial by clicking/pressing the key.
+             sequence: function(x){
+                    return [
+					                      // DEBUG INFORMATION
+                     // "<p style='font-size: small;'>Condition: "+x.condition+"; Item: "+x.item+"; Group: "+x.group+"</p>",
+					getPicture(x.PicFilename),
+
+                      // Wait 1sec
+                      {pause: 1000},
+                      // Enable clicks
+                      function(t){ t.enabled = true; },
+                      // Play audio file
+                      {audio: x.Sound_filename, type: "audio/wav", waitFor: true, newRT: true},
+					   {this: "answers", showKeys: "bottom", waitFor: true}
+                    ];
+                  }
+                }
+
+      ],
+
+
+
+      }
+
+
   }));
+
+  ["story",
